47390a7409db7c2a0d381971d8699f9cb8074bc9
77 <294195868@qq.com>
2025-06-30 18:20:44 +0800

    feat(Header): 添加搜索组件到头部布局
    新增 AppHeaderSearch 组件实现头部搜索功能，包含自动完成和过滤选项

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,10 +1,12 @@
 import { Layout, Card } from 'antd';
+import AppHeaderSearch from './Search';
 const { Header } = Layout;
 import './index.css'
 export default function AppHeader() {
   return (
     <Header style={{ background: 'transparent', padding: 0 }}>
       <Card className='header-card'>
+        <AppHeaderSearch />
       </Card>
     </Header>
   )
